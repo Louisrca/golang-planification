47a5f70dfc3e41188c0acac6048aa64f184babb5
Mat <matte971cr7cr8@gmail.com>
2024-01-26 13:32:58 +0100

    refactor password

--- repository/customer_repository.go
+++ repository/customer_repository.go
@@ -42,19 +42,23 @@ func GetCustomerByID(db *sql.DB, id string) (model.Customer, error) {
 }
 
 func CreateCustomer(db *sql.DB, customer model.Customer) (model.Customer, error) {
-	uuid := uuid.New()
+    uuid := uuid.New()
 
-	hashedPassword := utils.HashPassword(customer.Password)
-	_, err := db.Exec("INSERT INTO customer (id, firstname, lastname, email, password) VALUES (?, ?, ?, ?, ?)", uuid.String(), customer.Firstname, customer.Lastname, customer.Email, hashedPassword)
-	if err != nil {
-		log.Printf("Erreur lors de l'exécution de la requête: %v", err)
-		return model.Customer{}, err
-	}
+    // Hacher le mot de passe avant l'insertion
+    hashedPassword := utils.HashPassword(customer.Password)
+    customer.Password = hashedPassword // Mettre à jour l'objet customer avec le mot de passe haché
 
-	customer.ID = uuid.String()
+    // Insertion unique dans la base de données avec le mot de passe haché
+    _, err := db.Exec("INSERT INTO customer (id, firstname, lastname, email, password) VALUES (?, ?, ?, ?, ?)", uuid.String(), customer.Firstname, customer.Lastname, customer.Email, hashedPassword)
+    if err != nil {
+        log.Printf("Erreur lors de l'exécution de la requête: %v", err)
+        return model.Customer{}, err
+    }
 
-	return customer, nil
+    customer.ID = uuid.String()
+    return customer, nil
 }
+
 
 func UpdateCustomer(db *sql.DB, customer model.Customer) (model.Customer, error) {
 	_, err := db.Exec("UPDATE customer SET firstname = ?, lastname = ?, email = ? WHERE id = ?", customer.Firstname, customer.Lastname, customer.Email, customer.ID)
